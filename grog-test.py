3263ef682eefc646695680b68451a4ae60a3455a
Benjamin M. Gratch <bgratch@gmail.com>
2020-01-31 15:35:16 -0800

    Create grog-test.py
    program to test functionality

---
+++ grog-test.py
@@ -0,0 +1,8 @@
+# Program to test grogs functionality
+
+from grogs import Grog
+
+tyro = Grog('tyro', 40, 40, -4, -1, 1, ['40: no apparent aging'])
+tyro.display()
+nero = Grog('Nero', 25, 23, 0, 0, 0, [])
+nero.display()
