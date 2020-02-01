186dcc6ef5ef5516434777415cbe4fa7e9a3598b
Benjamin M. Gratch <bgratch@gmail.com>
2020-01-31 18:31:20 -0800

    Update grog-test.py

--- grog-test.py
+++ grog-test.py
@@ -1,8 +1,34 @@
 # Program to test grogs functionality
 
-from grogs import Grog
+import shelve, csv
+from grogs import Grog, csvGrog
+
+# grog format = Grog(name, age, appAge, ritual, ageMod, decrepitude, history)
 
 tyro = Grog('tyro', 40, 40, -4, -1, 1, ['40: no apparent aging'])
-tyro.display()
+#tyro.display()
 nero = Grog('Nero', 25, 23, 0, 0, 0, [])
-nero.display()
+#nero.display()
+
+
+grogFile = shelve.open('grogs')
+grogs = {}
+for grog in grogFile.keys():
+    grogs[grog.lower()] = grogFile[grog]
+    print('Importing:  ' + grog)
+
+grogs['tyro'].display()
+
+for grog in grogs.keys():
+    print('exporting %s...' % grog)
+    grogFile[grog] = grogs[grog]
+grogFile.close()
+
+
+# CSV test
+grogCSV = open('test-grogs.csv')
+grogReader = csv.reader(grogCSV)
+grogData = list(grogReader)
+print(grogData)
+gT = csvGrog(grogData[0])
+gT.display()
