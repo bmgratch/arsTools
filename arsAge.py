2ff37a0af22e6779dfceba561f4d12194ba91da7
Benjamin M. Gratch <bgratch@gmail.com>
2020-02-04 22:02:19 -0800

    Update arsAge.py
    ageSimple logic

--- arsAge.py
+++ arsAge.py
@@ -1,12 +1,46 @@
 # arsAge.py - aging program for Ars Magica grog tracking.
 # Planning on using this as a module in grogCraft2.py
 
+import math
 from grogs import Grog
 from arsRoll import roll
 
 # simple grog aging for non-statted grogs
 def ageSimple(grog, years=1):
-    pass
+    count = 0
+    history = grog.history
+    while count < years:
+        grog.age += 1
+        grog.appAge += 1
+        mod = math.ceil(grog.age / 10.0) + grog.ritual + grog.ageMod
+        ageRoll= roll() + mod
+        if grog.age < 35:
+            if ageRoll <= 2:
+                grog.appAge -= 1
+                grog.history.append('(%s) %s / %s'% (str(grog.age), ageTable[2], str(ageRoll)))
+        elif ageRoll <= 2:
+            grog.appAge -= 1
+            grog.history.append('(%s) %s / %s'% (str(grog.age), ageTable[2], str(ageRoll)))
+        elif ageRoll <= 9:
+            grog.history.append('(%s) %s / %s'% (str(grog.age), ageTable[3], str(ageRoll)))
+        elif ageRoll == 13:
+            grog.history.append('(%s) %s / %s'% (str(grog.age), ageTable[ageRoll], str(ageRoll)))
+            #grog.ritual = 0 #disabled
+        elif ageRoll <= 17:
+            grog.history.append('(%s) %s / %s'% (str(grog.age), ageTable[ageRoll], str(ageRoll)))
+            grog.agingPoints += 1
+        elif ageRoll <= 21:
+            grog.history.append('(%s) %s / %s'% (str(grog.age), ageTable[ageRoll], str(ageRoll)))
+            grog.agingPoints += 2
+        else:
+            grog.history.append('(%s) %s / %s'% (str(grog.age), ageTable[ageRoll], str(ageRoll)))
+            #grog.ritual = 0 #disabled
+        count += 1
+    for year in history:
+        print(' * %s' % year)
+        print()
+    return grog
+
 
 # processing a crisis, advancing aging points
 def crisis(ap):
@@ -14,30 +48,33 @@ def crisis(ap):
 
 # default age increase?
 ageTable = {
-    2 : "No Apparent Aging"
-    10: "1 aging point in any characteristic"
-    13: "CRISIS. Gain aging points to reach next decrepitude."
-    14: "1 aging point in Qik"
-    15: "1 aging point in Sta"
-    16: "1 aging point in Per"
-    17: "1 aging point in Prs"
-    18: "1 aging point each in Str and Sta"
-    19: "1 aging point each in Dex and Qik"
-    20: "1 aging point each in Com and Prs"
-    21: "1 aging point each in Int and Per"
+    2 : "No Apparent Aging",
+    3 : "Age increased",
+    10: "1 aging point in any characteristic",
+    13: "CRISIS. Gain aging points to reach next decrepitude.",
+    14: "1 aging point in Qik",
+    15: "1 aging point in Sta",
+    16: "1 aging point in Per",
+    17: "1 aging point in Prs",
+    18: "1 aging point each in Str and Sta",
+    19: "1 aging point each in Dex and Qik",
+    20: "1 aging point each in Com and Prs",
+    21: "1 aging point each in Int and Per",
     22: "CRISIS. Gain aging points to reach next decrepitude."
     }
 # crisis table?
 # simple die + age/10 + decrepitude
 crisisTable = {
-    8 : "Bedridden for a week."
-    9 : "Bedridden for a month."
-    15: "Minor Illness. Sta 3+ or CrCo 20"
-    16: "Serious Illness. Sta 6+ or CrCo 25
-    17: "Major Illness. Sta 9+ or CrCo 30"
-    18: "Critical Illness. Sta 12+ or CrCo 35."
+    8 : "Bedridden for a week.",
+    9 : "Bedridden for a month.",
+    15: "Minor Illness. Sta 3+ or CrCo 20",
+    16: "Serious Illness. Sta 6+ or CrCo 25",
+    17: "Major Illness. Sta 9+ or CrCo 30",
+    18: "Critical Illness. Sta 12+ or CrCo 35.",
     19: "Terminal Illness. CrCo 40."
     }
 
 # Grog loading and saving should be handled by grogCraft
 # tests below here.
+tyro = Grog('tyro', 40, 38, -6, -1, 11)
+ageSimple(tyro, 10)
