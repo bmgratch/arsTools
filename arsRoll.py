b73363779dbf5c6bd41c753d54a260f40d2ebfd4
bmgratch <bmgratch@users.noreply.github.com>
2020-02-04 18:16:42 -0800

    Create arsRoll.py
    stress die rolling module

---
+++ arsRoll.py
@@ -0,0 +1,18 @@
+# arsRoll.py - a die rolling module for Ars Magica uses
+
+# All the simulations could be done with random.randint()
+from random import randint
+
+
+def roll(b=0):
+    roll = randint(0,9)
+    #TODO botch dice. not needed for aging
+    # processing exploding dice
+    mult = 1
+    while roll == 1:
+        mult *= 2 # double on a 1, roll again
+        roll = randint(1, 10)
+    return(roll * mult)
+
+for i in range(0, 30):
+    print(roll())
