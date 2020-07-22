97add846b79b3dad9db7d5b2af834e48cb47e409
Benjamin M. Gratch <bgratch@gmail.com>
2020-07-22 13:27:50 -0700

    Create ageFlask.py

---
+++ ageFlask.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_me():
+    return "Hello, Mythic Europe!"
+
+@app.route('/hello/<cov>')
+def hello_name(cov):
+    return render_template('hello.html', name=cov)
+
+if __name__ == '__main__':
+    app.run(debug = True)
