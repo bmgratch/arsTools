6672e7682fc8d36bf563b00f3685f96669819d3b
Benjamin M. Gratch <bgratch@gmail.com>
2020-09-11 14:21:15 -0700

    load grogs from covenant.csv

--- ageFlask.py
+++ ageFlask.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template #? url_for
 import csv, arsAge, sys, os
-# from grogs import Grog, csvGrog
+from grogs import Grog, csvGrog
 
 # Constants
 COV_FOLDER = "covenants"
@@ -17,10 +17,24 @@ def index():
     covenants = [cov[:-4] for cov in os.listdir(COV_FOLDER) if cov.endswith('.tsv')]
     return render_template('index.html', covenants=covenants)
 
-@app.route('/cov/<name>')
-def covenant(name):
-    grogs = ['1','2']
-    return render_template('covenant.html', grogs=grogs, name=name)
+@app.route('/cov/<cov>')
+def covenant(cov):
+    grogs = loadCovenant(cov)
+    return render_template('covenant.html', grogs=grogs, cov=cov)
+
+def loadCovenant(covenant):
+    grogFile = open(os.path.join(COV_FOLDER, covenant + '.tsv'))
+    grogReader = csv.reader(grogFile, delimiter='\t')
+
+    cov_grogs = {}
+    grogData = list(grogReader)
+
+    for g in grogData:
+        cov_grogs[g[0].lower()] = csvGrog(g)
+        print('Importing: %s...' % g[0])
+    print('Import complete.')
+    grogFile.close()
+    return cov_grogs
 
 if __name__ == '__main__':
     app.run(debug = True)
