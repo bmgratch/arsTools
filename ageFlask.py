74d02a30721d98b6919da9961e08e9d63858bb6a
Benjamin M. Gratch <bgratch@gmail.com>
2020-09-10 18:38:31 -0700

    add basic format

--- ageFlask.py
+++ ageFlask.py
@@ -1,16 +1,26 @@
 from flask import Flask, render_template #? url_for
+import csv, arsAge, sys, os
+# from grogs import Grog, csvGrog
+
+# Constants
+COV_FOLDER = "covenants"
 
 app = Flask(__name__)
 
-grogs = ['1','2']
+## TODO Display listed grogs in covenant when selected
+## TODO Make buttons on COV do something
+## TODO actually update save data
+## TODO Make new covenants
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    covenants = [cov[:-4] for cov in os.listdir(COV_FOLDER) if cov.endswith('.tsv')]
+    return render_template('index.html', covenants=covenants)
 
 @app.route('/cov/<name>')
-def hello_name(name):
-    return render_template('covenant.html', grogs=grogs)
+def covenant(name):
+    grogs = ['1','2']
+    return render_template('covenant.html', grogs=grogs, name=name)
 
 if __name__ == '__main__':
     app.run(debug = True)
